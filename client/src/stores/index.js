f44d976d1fc364609a916876657d5abdc0c3182e
dibazalfa <dibazalfa@gmail.com>
2023-06-12 21:37:31 +0700

    commit

--- client/src/stores/index.js
+++ client/src/stores/index.js
@@ -11,18 +11,6 @@ export const useApp = defineStore({
       privateKey: "",
   }),
   actions: {
-    async generateKey() {
-      const q = this.key;
-      // console.log(q);
-      
-      try {
-        const response = await axios.post('http://localhost:5000/api/data', { q });
-        console.log(response.data.key); // Tanggapan dari server
-        this.getData();
-      } catch (error) {
-        console.log(error);
-      }
-    },
     async getData() {
       console.log("halo ini get data")
       try {
@@ -34,38 +22,38 @@ export const useApp = defineStore({
            // Membuat file teks
           const publicKeyBlob = new Blob([this.publicKey], { type: 'text/plain' });
           const privateKeyBlob = new Blob([this.privateKey], { type: 'text/plain' });
-
+  
           // Membuat URL objek blob
           const publicKeyURL = window.URL.createObjectURL(publicKeyBlob);
           const privateKeyURL = window.URL.createObjectURL(privateKeyBlob);
-
+  
           // Membuat elemen anchor untuk mengunduh file
           const publicKeyLink = document.createElement('a');
           publicKeyLink.href = publicKeyURL;
           publicKeyLink.download = this.key + '.pub';
-
+  
           const privateKeyLink = document.createElement('a');
           privateKeyLink.href = privateKeyURL;
           privateKeyLink.download = this.key + '.pri';
-
+  
           // Menyembunyikan elemen anchor dan mengkliknya
           publicKeyLink.style.display = 'none';
           privateKeyLink.style.display = 'none';
           document.body.appendChild(publicKeyLink);
           document.body.appendChild(privateKeyLink);
-
+  
           publicKeyLink.click();
           privateKeyLink.click();
-
+  
           // Menghapus URL objek blob
           window.URL.revokeObjectURL(publicKeyURL);
           window.URL.revokeObjectURL(privateKeyURL);
-
+  
           // console.log("Public Key:", this.publicKey);
           // console.log("Private Key:", this.privateKey);
       } catch (error) {
         console.log(error)
       }
-    }
+    },
   }
 })
--- client/src/views/Validator.vue
+++ client/src/views/Validator.vue
@@ -9,7 +9,10 @@
       <!-- untuk upload file .pdf -->
       <label>File Input</label>
       <input type="text">
-      <button>Browse</button> 
+      <button>Browse</button>
+      <!-- <input type="file" ref="fileInput" @change="App.handleFileChange"> -->
+      <!-- <button @click="App.uploadFile()">Unggah</button>  -->
+      
       <br>
       <label>File Key</label>
       <input type="text">
@@ -22,3 +25,18 @@
     </div>
   </div>
 </template>
+
+<script>
+import { useApp } from "../stores/index";
+export default {
+  setup() {
+    const App = useApp();
+    return {
+      App,
+    };
+  },
+  mounted() {
+    
+  }
+};
+</script>
