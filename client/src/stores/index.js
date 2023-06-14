76ed8c8d4c83684b2cd93b6a66b4b36011222a2f
dibazalfa <dibazalfa@gmail.com>
2023-06-14 11:50:54 +0700

    merger-val

--- client/src/stores/index.js
+++ client/src/stores/index.js
@@ -85,11 +85,9 @@ export const useApp = defineStore({
       try {
         const formData = new FormData();
         formData.append('filename', this.file);
-        formData.append('publicKey', this.publicKey);
-        formData.append('publicKey', this.publicKey);
+        formData.append('privatekey', this.privatekey);
         console.log(this.file);
-        console.log(this.publicKey);
-        console.log(this.publicKey);
+        console.log(this.privatekey);
 
         const response = await axios.post('http://localhost:5000/api/sign', formData);
         const { message, signed_filename } = response.data;
@@ -103,48 +101,6 @@ export const useApp = defineStore({
       } catch (error) {
         console.log(error);
       }
-    },
-    async submitvalidator() {
-      try {
-        const formData = new FormData();
-        formData.append('filename', this.file);
-        formData.append('privatekey', this.privatekey);
-        formData.append('Sign', this.Sign);
-        console.log(this.file);
-        console.log(this.Sign);
-
-        const response = await axios.post('http://localhost:5000/api/validation', formData);
-        const { message, signed_filename } = response.data;
-        console.log(signed_filename);
-        if (signed_filename) {
-          alert(message);
-          this.isSubmitted = true;
-        } else {
-          console.log('Signed file not falid');
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    inputPublicKey(publicKey) {
-      if (publicKey) {
-        console.log('publicKey key dipilih:', publicKey.name);
-        this.publicKey = publicKey;
-      }
-    },
-    browsePublicKey() {
-      const publicKeyInput = document.querySelectorAll('input[type="file"]')[1];
-      publicKeyInput.click();
-    },
-    inputSign(Sign) {
-      if (Sign) {
-        console.log('Sign key dipilih:', Sign.name);
-        this.Sign = Sign;
-      }
-    },
-    browseSign() {
-      const inputSign = document.querySelectorAll('input[type="file"]')[1];
-      inputSign.click();
     },
   },
 });
